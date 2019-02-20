cd70825389089b0bb0c913dda86afdb5fa1bf70e
Rafał Olszewicz <rafalolszewicz@gmail.com>
2019-02-20 07:04:45 +0100

    update content categories, tags

--- templates/views/blog/blog-categories.php
+++ templates/views/blog/blog-categories.php
@@ -4,21 +4,20 @@
 
 <div id='content-body' pw-append>
 
-<div class='uk-flex uk-flex-around'>
+<div class='uk-flex uk-flex-wrap uk-padding' uk-grid>
   <?php foreach($categories as $category): ?>
-    <?php editItem($category);?>
-    <a href='<?=$category->url?>'>
-    <div class='uk-card uk-card-default uk-card-hover uk-card-body'>
-          <h3><?=$category->title?>
-            <span style='border-top: 3px solid black;
-                  border-left: 3px solid black; border-radius: 50%; padding: 5px 15px;'
-                  class='count-category'><?=count($category->references())?>
-            </span>
-          </h3>
-    </div>
-  </a>
+  <?php editItem($category);?>
+    <a class='uk-padding-small hover' href='<?=$category->url?>'>
+      <div class='uk-card uk-card-default uk-card-hover uk-card-body uk-margin-small'>
+            <h3><?=$category->title?>
+              <span style='border-top: 3px solid black;
+                    border-left: 3px solid black; border-radius: 50%; padding: 5px 15px;'
+                    class='count-category'><?=count($category->references())?>
+              </span>
+            </h3>
+      </div>
+    </a>
   <?php endforeach; ?>
-
 </div>
 
 <?=ukPagination($categories); ?>
--- templates/views/blog/blog-tags.php
+++ templates/views/blog/blog-tags.php
@@ -4,21 +4,20 @@
 
 <div id='content-body' pw-append>
 
-<div class='uk-flex uk-flex-around'>
+<div class='uk-flex uk-flex-wrap uk-padding' uk-grid>
   <?php foreach($tags as $tag): ?>
-    <div>
+    <a href='<?=$tag->url?>' class='hover'>
+      <div class='uk-margin-small'>
           <?php editItem($tag);?>
-          <a href='<?=$tag->url?>' class='uk-inline'>
           <h3><?=$tag->title?>
             <span style='border-top: 3px solid black;
                   border-left: 3px solid black; border-radius: 50%; padding: 5px 15px;'
                   class='count-category'><?=count($tag->references())?>
             </span>
           </h3>
-        </a>
-    </div>
+      </div>
+    </a>
   <?php endforeach; ?>
-
 </div>
 
 <?=ukPagination($tags); ?>
